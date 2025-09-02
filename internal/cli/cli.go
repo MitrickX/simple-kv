7cffad6d9b01f8e8dcca897a81147ba819ae773d
Dmitry Pavlov <mitrickx@mail.ru>
2025-09-02 19:43:59 +0300

    cli db

---
+++ internal/cli/cli.go
@@ -0,0 +1,44 @@
+package cli
+
+import (
+	"bufio"
+	"io"
+
+	"github.com/MitrickX/simple-kv/internal/db"
+)
+
+type Cli struct {
+	scanner *bufio.Scanner
+	db      *db.DB
+	out     io.Writer
+	errOut  io.Writer
+}
+
+func NewCli(
+	reader io.Reader,
+	out io.Writer,
+	errOut io.Writer,
+	db *db.DB,
+) *Cli {
+	scanner := bufio.NewScanner(reader)
+	return &Cli{
+		scanner: scanner,
+		db:      db,
+		out:     out,
+		errOut:  errOut,
+	}
+}
+
+func (cli *Cli) Go() {
+	for cli.scanner.Scan() {
+		query := cli.scanner.Text()
+		result, err := cli.db.Exec(query)
+		if err != nil {
+			cli.errOut.Write([]byte(err.Error()))
+			continue
+		}
+
+		cli.out.Write([]byte(result))
+		cli.out.Write([]byte{'\n'})
+	}
+}
