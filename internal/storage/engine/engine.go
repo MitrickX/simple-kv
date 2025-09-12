96169f0b878f096de7f73e223c49b1509395958e
Dmitry Pavlov <mitrickx@mail.ru>
2025-09-12 21:33:20 +0300

    rwmutex

--- internal/storage/engine/engine.go
+++ internal/storage/engine/engine.go
@@ -1,5 +1,7 @@
 package engine
 
+import "sync"
+
 type Engine interface {
 	Set(key, value string)
 	Get(key string) (string, bool)
@@ -7,23 +9,31 @@ type Engine interface {
 }
 
 type engine struct {
+	mx *sync.RWMutex
 	kv map[string]string
 }
 
 func NewEngine() Engine {
 	return &engine{
+		mx: &sync.RWMutex{},
 		kv: make(map[string]string),
 	}
 }
 
 func (e *engine) Set(key, value string) {
+	defer e.mx.Unlock()
+	e.mx.Lock()
 	e.kv[key] = value
 }
 func (e *engine) Get(key string) (string, bool) {
+	defer e.mx.RUnlock()
+	e.mx.RLock()
 	val, ok := e.kv[key]
 	return val, ok
 }
 
 func (e *engine) Del(key string) {
+	defer e.mx.Unlock()
+	e.mx.Lock()
 	delete(e.kv, key)
 }
